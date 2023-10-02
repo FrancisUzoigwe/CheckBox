07933c77e896a584c18ddec577082d0d7251f2e9
FrancisUzoigwe <kossyuzoigwe@gmail.com>
2023-10-02 10:49:28 +0100

    checked

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import { useState } from "react"
+
+const App = () => {
+
+  const [checked, setChecked] = useState<boolean>(false)
+  return (
+    <div className="bg-gray-400 w-full h-screen flex items-center justify-center">
+      <div className="min-w-[200px] min-h-[200px] rounded-lg bg-white flex justify-center items-center">
+        <div className="flex flex-col">
+          <input type="checkbox" onClick={(e:any) => {
+            setChecked(e.target.checked)
+          }}/>
+          <button className={`mt-4 px-7 py-3 ${checked? "bg-purple-400" : "bg-gray-300"} text-white font-semibold  rounded-xl`} disabled={!checked}>Proceed</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
